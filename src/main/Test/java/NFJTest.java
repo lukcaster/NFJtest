6d381721a2ac98147cb937f6b254dc1185fd0f7c
lukcaster <gruchala.lukasz1@gmail.com>
2021-02-26 07:26:26 +0100

    Working on assertion to see if i am logged in, looking for solution

--- src/main/Test/java/NFJTest.java
+++ src/main/Test/java/NFJTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
 import java.sql.Time;
@@ -34,12 +35,22 @@ public class NFJTest {
         driver.findElement(By.xpath("/html/body/nfj-root/nfj-layout/div/ngx-loadable/nfj-lazy-layout/nfj-fixed-banner/section/nfj-cookie-information/div/div/button")).click();
 
         //Check if I am logged in
-        //String Email = driver.findElement(By.xpath("//*[@id=\"navbarNav\"]/ul[2]/li[3]/common-lazy-render/nfj-navbar-candidate-wrapper/nfj-navbar-profile-dropdown/div/span/span")).getAttribute();
-        //if (driver.findElement(By.xpath("//*[@id=\"navbarNav\"]/ul[2]/li[3]/common-lazy-render/nfj-navbar-candidate-wrapper/nfj-navbar-profile-dropdown/div/span/span".));
-) {
-        } else {
-            Assert.fail();
+        String MyEmail = "gruchala.lukasz1@gmail.com";
+        driver.findElement(By.xpath("//*[@id=\"navbarNav\"]/ul[2]/li[3]/common-lazy-render/nfj-navbar-candidate-wrapper/nfj-navbar-profile-dropdown/div/span/span")).click();
+
+        if (driver.findElement(By.xpath("//*[@id=\"navbar-login\"]/span")).isEnabled())
+        {
+            driver.findElement(By.xpath("//*[@id=\"navbar-login\"]/span.click")).click();
+            TimeUnit.SECONDS.sleep(1);
+            driver.findElement(By.xpath("/html/body/nfj-root/nfj-layout/nfj-auth-loader/ngx-loadable/nfj-auth-handler/div/div/nfj-auth-login/nfj-auth-shell/nfj-social-login-box/div/div[4]/button")).click();
+            driver.findElement(By.xpath("//*[@id=\"profileIdentifier\"]")).click();
+        } else {Assert.fail();
         }
+
+
+
+
+
     }
 }
 
