a1bb54bc90fdafa784e158c7b25b0e89c6fa0091
lukcaster <gruchala.lukasz1@gmail.com>
2021-03-01 19:19:29 +0100

    Hovering over Log in button seems to help, but test doesnt work always

--- src/main/Test/java/NFJTest.java
+++ src/main/Test/java/NFJTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import javax.swing.*;
 import java.awt.*;
@@ -31,14 +32,22 @@ public class NFJTest {
     @Test
     public void NFJMainTest() throws InterruptedException, IOException, AWTException {
         //Cookies
-
-        driver.findElement(By.xpath("//*[@id=\"navbarNav\"]/ul[2]/li[3]/common-lazy-render/nfj-navbar-login-wrapper/nfj-navbar-profile-dropdown")).click();
+        Actions LogInAction = new Actions(driver);
+        WebElement LogInWebElement = driver.findElement(By.xpath("//*[@id=\"navbarNav\"]/ul[2]/li[3]"));
+        LogInAction.moveToElement(LogInWebElement).build().perform();
+        LogInWebElement.click();
+        WebElement LogInWebElementClick = driver.findElement(By.xpath("//*[@id=\"navbarNav\"]/ul[2]/li[3]/common-lazy-render/nfj-navbar-candidate-wrapper/nfj-navbar-profile-dropdown/div/span/span"));
+        LogInWebElementClick.click();
+        WebElement LogInWebElement1 = driver.findElement(By.xpath("//*[@id=\"navbar-login\"]/span"));
+        LogInWebElement1.isEnabled();
+        LogInWebElement1.click();
+        //driver.findElement(By.xpath("//*[@id=\"navbarNav\"]/ul[2]/li[3]/common-lazy-render/nfj-navbar-candidate-wrapper/nfj-navbar-profile-dropdown/div")).click();
 
         //Check if I am logged in
         //String MyEmail = "gruchala.lukasz1@gmail.com";
-        driver.findElement(By.xpath("//*[@id=\"navbarNav\"]/ul[2]/li[3]/common-lazy-render/nfj-navbar-login-wrapper/nfj-navbar-profile-dropdown/div/span")).click();
-        WebElement LogIn = driver.findElement(By.xpath("//*[@id=\"navbarNav\"]/ul[2]/li[3]/common-lazy-render/nfj-navbar-login-wrapper/nfj-navbar-profile-dropdown/div/span/span"));
-        System.out.print(LogIn);
+        //driver.findElement(By.xpath("//*[@id=\"navbarNav\"]/ul[2]/li[3]/common-lazy-render/nfj-navbar-login-wrapper/nfj-navbar-profile-dropdown/div/span")).click();
+        //WebElement LogIn = driver.findElement(By.xpath("//*[@id=\"navbarNav\"]/ul[2]/li[3]/common-lazy-render/nfj-navbar-login-wrapper/nfj-navbar-profile-dropdown/div/span/span"));
+        //System.out.print(LogIn);
 
         }
 
