6327c25f66456a769117bf8a5440773626f73205
lukcaster <gruchala.lukasz1@gmail.com>
2021-03-03 10:33:22 +0100

    Cleaned the code a little bit, cannot figure out a way to type in the email

--- src/main/Test/java/NFJTest.java
+++ src/main/Test/java/NFJTest.java
@@ -38,33 +38,19 @@ public class NFJTest {
         WebElement LogInWebElement = driver.findElement(By.xpath("//*[@id=\"navbarNav\"]/ul[2]/li[3]"));
         LogInAction.moveToElement(LogInWebElement).build().perform();
         LogInWebElement.click();
-        //WebElement LogInWebElementClick = driver.findElement(By.xpath("//*[@id=\"navbarNav\"]/ul[2]/li[3]/common-lazy-render/nfj-navbar-candidate-wrapper/nfj-navbar-profile-dropdown/div/span/span"));
-        //LogInWebElementClick.click();
         TimeUnit.SECONDS.sleep(2);
         WebElement LogInWebElement1 = driver.findElement(By.xpath("//*[@id=\"navbar-login\"]/span"));
         if(LogInWebElement1.isEnabled());
         LogInWebElement1.click();
-        //TimeUnit.SECONDS.sleep(1);
-        //driver.findElement(By.xpath("//*[@id=\"navbarNav\"]/ul[2]/li[3]/common-lazy-render/nfj-navbar-candidate-wrapper/nfj-navbar-profile-dropdown/div")).click();
         TimeUnit.SECONDS.sleep(2);
         WebElement FakeEmailWebElement = driver.findElement(By.xpath("/html/body/nfj-root/nfj-layout/nfj-auth-loader/ngx-loadable/nfj-auth-handler/div/div/nfj-auth-login/nfj-auth-shell/nfj-social-login-box/div/div[4]/button"));
         FakeEmailWebElement.click();
         TimeUnit.SECONDS.sleep(2);
         String FakeEmail = "email123@gmail.com";
-        WebElement FakeEmailWebElement1 = driver.findElement(By.xpath("//*[@id=\"identifierId\"]"));
+        FakeEmailWebElement.sendKeys(FakeEmail);
+        WebElement FakeEmailWebElement1 = driver.findElement(By.xpath("//*[@id=\"view_container\"]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[3]"));
         FakeEmailWebElement1.sendKeys(FakeEmail);
 
-        //WebDriverWait Wait = new WebDriverWait(WebDriverRefernce,20);
-        //TimeUnit.SECONDS.sleep(2);
-        //WebElement FakeEmailWebElement = driver.findElement(By.xpath("//*[@id=\"view_container\"]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[3]"));
-        //FakeEmailWebElement.click();
-        //FakeEmailWebElement.sendKeys(FakeEmail);
-
-        //Check if I am logged in
-        //String MyEmail = "gruchala.lukasz1@gmail.com";
-        //driver.findElement(By.xpath("//*[@id=\"navbarNav\"]/ul[2]/li[3]/common-lazy-render/nfj-navbar-login-wrapper/nfj-navbar-profile-dropdown/div/span")).click();
-        //WebElement LogIn = driver.findElement(By.xpath("//*[@id=\"navbarNav\"]/ul[2]/li[3]/common-lazy-render/nfj-navbar-login-wrapper/nfj-navbar-profile-dropdown/div/span/span"));
-        //System.out.print(LogIn);
 
         }
 
