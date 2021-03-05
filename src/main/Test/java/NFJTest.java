94e243b79b6f9f6275add3305522799be8dd97b9
lukcaster <gruchala.lukasz1@gmail.com>
2021-03-05 13:47:47 +0100

    Created a fake email for logging purpose, added @else to the test, only thing left is creating a search functions

--- src/main/Test/java/NFJTest.java
+++ src/main/Test/java/NFJTest.java
@@ -2,10 +2,12 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -26,33 +28,45 @@ public class NFJTest {
         setProperty("webdriver.chrome.driver",
                 "src/main/resources/driver/chromedriver.exe");
         driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("https://nofluffjobs.com/pl");
-        TimeUnit.SECONDS.sleep(1);
+
     }
 
     @Test
     public void NFJMainTest() throws InterruptedException, IOException, AWTException {
-        //Cookies
+
+        WebDriverWait wait = new WebDriverWait(driver, 30);
         Actions LogInAction = new Actions(driver);
         WebElement LogInWebElement = driver.findElement(By.xpath("//*[@id=\"navbarNav\"]/ul[2]/li[3]"));
         LogInAction.moveToElement(LogInWebElement).build().perform();
         LogInWebElement.click();
-        TimeUnit.SECONDS.sleep(2);
         WebElement LogInWebElement1 = driver.findElement(By.xpath("//*[@id=\"navbar-login\"]/span"));
-        if(LogInWebElement1.isEnabled());
+        wait.until(ExpectedConditions.elementToBeClickable(LogInWebElement1));
+        if(LogInWebElement1.isEnabled()){
         LogInWebElement1.click();
-        TimeUnit.SECONDS.sleep(2);
         WebElement FakeEmailWebElement = driver.findElement(By.xpath("/html/body/nfj-root/nfj-layout/nfj-auth-loader/ngx-loadable/nfj-auth-handler/div/div/nfj-auth-login/nfj-auth-shell/nfj-social-login-box/div/div[4]/button"));
-        FakeEmailWebElement.click();
-        TimeUnit.SECONDS.sleep(2);
-        String FakeEmail = "email123@gmail.com";
-        FakeEmailWebElement.sendKeys(FakeEmail);
-        WebElement FakeEmailWebElement1 = driver.findElement(By.xpath("//*[@id=\"view_container\"]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[3]"));
-        FakeEmailWebElement1.sendKeys(FakeEmail);
-
-
+        wait.until(ExpectedConditions.elementToBeClickable(FakeEmailWebElement));
+        String FakeEmail = "jmuzykant331@gmail.com";
+        String FakePassword = "Test!234";
+        WebElement FakeEmailInput = driver.findElement(By.xpath("//*[@id=\"log-in-email\"]"));
+        WebElement FakePasswordInput = driver.findElement(By.xpath("//*[@id=\"log-in-password\"]"));
+        FakeEmailInput.click();
+        FakeEmailInput.clear();
+        FakeEmailInput.sendKeys(FakeEmail);
+        FakePasswordInput.click();
+        FakePasswordInput.clear();
+        FakePasswordInput.sendKeys(FakePassword);
+        driver.findElement(By.xpath("//*[@id=\"log-in-submit\"]")).click();
+        } else{
+            WebElement MyEmail = driver.findElement(By.xpath("//*[@id=\"navbarNav\"]/ul[2]/li[3]/common-lazy-render/nfj-navbar-candidate-wrapper/nfj-navbar-profile-dropdown/div/div/nfj-navbar-candidate/header/p"));
+            MyEmail.isEnabled();
         }
+
+
+    }
 
 
 
